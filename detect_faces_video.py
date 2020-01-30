2ca50ef2f5b06acf7448ad4f6a851d1bbf3b989c
Merin <hackerv727@gmail.com>
2020-01-30 23:03:13 +0545

    Update detect_faces_video.py

--- detect_faces_video.py
+++ detect_faces_video.py
@@ -3,7 +3,6 @@
 import cv2  
 import tkinter as tk
 # load the required trained XML classifiers 
-# https://github.com/Itseez/opencv
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')  
 smile_cascade=cv2.CascadeClassifier('haarcascade_smile.xml')
